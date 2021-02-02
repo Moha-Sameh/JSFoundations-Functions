badaa6f11a0a7d3efee87a75d2a43206d537d284
@Moha-Sameh <ms.slama01@hotmail.com>
2021-02-03 00:08:41 +0200

    Task done

--- functions.js
+++ functions.js
@@ -1,53 +1,25 @@
-/**
- * greet(name):
- * - receives a name
- * - logs "Hello <name>"
- *
- * e.g.
- * greet("Hamza") logs "Hello Hamza"
- */
-function greet(name) {
-  // Your code here
+function greet(Receivename) {
+  console.log(`Hello ${Receivename}`);
 }
-
-/**
- * isOdd(n):
- * - receives a number n
- * - returns true if it's odd, false otherwise
- *
- * e.g.
- * isOdd(7) -> true
- * isOdd(10) -> false
- */
 function isOdd(n) {
-  // Your code here
+  if (n % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
-
-/**
- * oddsSmallerThan(n):
- * - receives a number n
- * - returns the number of ODD numbers smaller than n
- *
- * e.g.
- * oddsSmallerThan(7) -> 3
- * oddsSmallerThan(15) -> 7
- */
 function oddsSmallerThan(n) {
-  // Your code here
+  if (n % 2 === 1) {
+    return (n - 1) / 2;
+  } else {
+    return n / 2;
+  }
 }
-
-/**
- * squareOrDouble(n):
- * - receives a number n
- * - returns its square if it's odd
- * - returns its double if it's even
- *
- * e.g.
- * squareOrDouble(16) -> 32
- * squareOrDouble(9) -> 81
- */
 function squareOrDouble(n) {
-  // Your code here
+  if (n % 2 === 1) {
+    return Math.pow(n, 2);
+  } else {
+    return n * 2;
+  }
 }
-
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
